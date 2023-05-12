ef3d60e5232a35468da061f068702df463818731
Hanz Bobot <132050914+OrewaHanz@users.noreply.github.com>
2023-05-12 21:02:15 +0800

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `botrimuru onz 🫰🏻`
+let info = `Syaa Onzz! (≧∀≦)`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 /*const message = {
